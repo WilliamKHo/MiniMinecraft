58485c735e3c094974085e31823c5eaf77077809
WilliamKHo <willho@seas.upenn.edu>
2018-01-14 14:44:52 -0500

    Initial Commit

---
+++ MiniMinecraft/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MiniMinecraft
+//
+//  Created by William Ho on 1/14/18.
+//  Copyright © 2018 William Ho. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MiniMinecraft/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MiniMinecraft
+//
+//  Created by William Ho on 1/14/18.
+//  Copyright © 2018 William Ho. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
